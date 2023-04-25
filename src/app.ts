2300842173b2b052191c151dc4fdeb42d9d33a87
Tiago Gomes <tidgomes@gmail.com>
2023-04-25 12:40:39 -0300

    use keyof to identify if a certain key is from a given object

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-interface Lengthy {
-  length: number
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value ${obj[key]}`
 }
 
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let description = 'Got no elements'
-  if (element.length === 1) description = 'Got 1 element'
-  else if (element.length > 1) description = `Got ${element.length} elements`
-  return [element, description]
-}
-
-console.log(countAndDescribe('Hello there!'))
-console.log(countAndDescribe(['Sports', 'Videogames']))
+const value = extractAndConvert({ name: 'mano' }, 'name')
+console.log(value)
