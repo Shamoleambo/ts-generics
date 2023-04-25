ba405dfd50037cc0a66516b5a02dd755ed855783
Tiago Gomes <tidgomes@gmail.com>
2023-04-25 12:26:37 -0300

    add constrains to generic types

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-const names: Array<string> = []
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
+}
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!')
-  }, 2000)
-})
-
-promise.then((data) => data.split(' '))
+const obj = merge({ name: 'mano' }, { age: 27 })
+const obj2 = merge({ name: 'truta' }, { age: 22 })
+console.log(obj.name)
+console.log(obj2.name)
