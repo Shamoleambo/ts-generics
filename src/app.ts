87c58210ac19979044677d8655be4b7d1cad9d28
Tiago Gomes <tidgomes@gmail.com>
2023-04-25 12:02:24 -0300

    allow undefined parameters

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const names: Array<string> = []
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is done!')
+  }, 2000)
+})
+
+promise.then((data) => data.split(' '))
